b85c3fe5a380a0dd8fe748679bf70d35ce176338
rwr1 <vijayDM6258@gmail.com>
2024-10-10 10:05:30 +0530

    url,share,auth

--- lib/view/contact_page.dart
+++ lib/view/contact_page.dart
@@ -3,7 +3,10 @@ import 'package:contact/provider/contact_provider.dart';
 import 'package:contact/util.dart';
 import 'package:contact/util/global.dart';
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
@@ -51,15 +54,58 @@ class _ContactPageState extends State<ContactPage> {
                   children: [
                     IconButton(
                         onPressed: () {
+                          launchUrl(Uri.parse("tel:${contact.number}"));
+                        },
+                        icon: Icon(Icons.call)),
+                    PopupMenuButton(
+                      itemBuilder: (context) {
+                        return [
+                          PopupMenuItem(
+                            child: Row(
+                              children: [Icon(Icons.edit), Text("Edit")],
+                            ),
+                            value: "edit",
+                          ),
+                          PopupMenuItem(
+                            child: Row(
+                              children: [Icon(Icons.delete), Text("Delete")],
+                            ),
+                            value: "delete",
+                          ),
+                          PopupMenuItem(
+                            child: Row(
+                              children: [Icon(Icons.share), Text("Share")],
+                            ),
+                            value: "share",
+                          ),
+                          PopupMenuItem(
+                            child: Row(
+                              children: [Icon(Icons.mail), Text("Email")],
+                            ),
+                            value: "mail",
+                          ),
+                        ];
+                      },
+                      onSelected: (val) {
+                        print("onSelected $val");
+                        if (val == "edit") {
                           Navigator.pushNamed(context, PageName.addContact,
                               arguments: index);
-                        },
-                        icon: Icon(Icons.edit)),
-                    IconButton(
-                        onPressed: () {
+                        }
+                        if (val == "delete") {
                           value.removeContact(index);
-                        },
-                        icon: Icon(Icons.delete)),
+                        }
+                        if (val == "mail") {
+                          launchUrl(Uri.parse("mailto:${contact.email}"));
+                        }
+                        if (val == "share") {
+                          String shareText =
+                              "Please call me i am waiting ${contact.number}";
+                          Share.share(shareText);
+                          Share.shareXFiles([XFile("path")]);
+                        }
+                      },
+                    )
                   ],
                 ),
               );
@@ -68,8 +114,19 @@ class _ContactPageState extends State<ContactPage> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, PageName.addContact);
+        onPressed: () async {
+          LocalAuthentication authentication = LocalAuthentication();
+          if (await authentication.isDeviceSupported()) {
+            print("isDeviceSupported");
+            bool isAuth = await authentication.authenticate(
+                localizedReason: "Please authenticate to add Contact");
+            if (isAuth) {
+              Navigator.pushNamed(context, PageName.addContact);
+            }
+            print("isAuth $isAuth");
+          } else {
+            print("isDeviceSupported false");
+          }
         },
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import local_auth_darwin
+import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
